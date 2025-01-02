39ea828cdd71e674aa4596aed48cb672bb153bce
Anwar Ahmed <anwar.ahmed@gmail.com>
2025-01-01 22:05:30 -0500

    2024 12 31 (#2)
    * added copilot
    
    * fixed issue with conform stylua formatter inserting tabs instead of spaces

--- .config/nvim/lua/anwar/plugins/lsp/mason.lua
+++ .config/nvim/lua/anwar/plugins/lsp/mason.lua
@@ -1,50 +1,51 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
 
-		local mason_tool_installer = require("mason-tool-installer")
+    -- import mason-tool-installer
+    local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"csharp_ls",
-				"omnisharp",
-				"ts_ls",
-				"eslint",
-				"html",
-				"cssmodules_ls",
-				"tailwindcss",
-				"lua_ls",
-			},
-		})
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        "csharp_ls",
+        --"omnisharp",
+        "ts_ls",
+        "eslint",
+        "html",
+        "cssmodules_ls",
+        "tailwindcss",
+        "lua_ls",
+      },
+    })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"csharpier", -- c# formatter
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"eslint_d", -- js linter
-			},
-		})
-	end,
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "csharpier", -- c# formatter
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "eslint_d", -- js linter
+      },
+    })
+  end,
 }
