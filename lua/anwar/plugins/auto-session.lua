ed1d1af6d110b35b131ba63d61e857433e889cfe
Anwar Ahmed <anwar.ahmed@gmail.com>
2025-11-11 17:14:43 -0500

    updated auto-session config to not store session in ~/.config/* (#41)

--- lua/anwar/plugins/auto-session.lua
+++ lua/anwar/plugins/auto-session.lua
@@ -5,7 +5,7 @@ return {
 
     auto_session.setup({
       auto_restore = true,
-      suppressed_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+      suppressed_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/", "~/.config/*" },
     })
 
     local keymap = vim.keymap
